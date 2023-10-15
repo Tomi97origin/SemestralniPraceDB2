c17f3caefe6d04cb1e0a61942d0115e06faf05d9
Tomáš Patzák <st67084@upce.cz>
2023-10-15 21:38:43 +0200

    Funkcni pripojeni k DB

--- SemestralniPraceDB2/Models/DatabaseConnector.cs
+++ SemestralniPraceDB2/Models/DatabaseConnector.cs
@@ -12,29 +12,21 @@ namespace SemestralniPraceDB2.Models
     public class DatabaseConnector
     {
 
-
+        public static OracleConnection GetConnection()
+        {
+            string connectionString = "User Id=st67084;Password=semestralkadb2;Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=fei-sql3.upceucebny.cz)(PORT=1521))(CONNECT_DATA=(SID=BDAS)(SERVER=DEDICATED)))";
+            return new OracleConnection(connectionString);
+        }
         public static string GetFromDatabase()
         {
             string? dbResult = string.Empty;
-
-            string username = "st64515";
-            string password = "hodiny";
-            string hostname = "fei-sql3.upceucebny.cz";
-            int port = 1521; // Oracle default port
-            string sid = "BDAS";
-
-            string connectionString = $"Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST={hostname})(PORT={port}))(CONNECT_DATA=(SID={sid})));User Id={username};Password={password};";
-
-
-            //    string connectionString = "Data Source=fei-sql3.upceucebny.cz;User Id=st67084;Password=semestralkadb2;";
-            OracleConnection connection = new OracleConnection(connectionString); //ConfigurationManager.ConnectionStrings["mojePripojeni"].ConnectionString);
-
+            OracleConnection connection = GetConnection();
             try
             {
                 connection.Open();
                 // Spojení bylo úspěšně navázáno
 
-                string query = "SELECT * FROM AUTO";
+                string query = "SELECT * FROM NEW_DEPTS";
                 OracleCommand command = new OracleCommand(query, connection);
 
                 try
