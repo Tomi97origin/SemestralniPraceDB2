b830ffa94adc2bedbcba62754e759faf2dd39944
Tomáš Patzák <st67084@upce.cz>
2023-11-25 00:59:43 +0100

    formatovani

--- SemestralniPraceDB2/Models/SystemCatalogService.cs
+++ SemestralniPraceDB2/Models/SystemCatalogService.cs
@@ -23,12 +23,12 @@ namespace SemestralniPraceDB2.Models
                     {
                         string sql = "SELECT t.* FROM user_tables t";
                         List<OracleParameter> prm = new();
-                        var result = DatabaseConnector.ExecuteCommandQueryForTransactionAsync(sql, prm,connection, MapOracleResultToTable).Result;
-                        foreach(DBTable table in result)
+                        var result = DatabaseConnector.ExecuteCommandQueryForTransactionAsync(sql, prm, connection, MapOracleResultToTable).Result;
+                        foreach (DBTable table in result)
                         {
-                            sql = "SELECT count(*) as  row_count  FROM " + table.TableName+" t";
+                            sql = "SELECT count(*) as  row_count  FROM " + table.TableName + " t";
                             prm = new();
-                            var res = DatabaseConnector.ExecuteCommandQueryForTransactionAsync(sql, prm, connection, (reader) => reader.GetInt32("row_count") ).Result;
+                            var res = DatabaseConnector.ExecuteCommandQueryForTransactionAsync(sql, prm, connection, (reader) => reader.GetInt32("row_count")).Result;
                             table.RowCount = res[0];
                         }
                         tables.AddRange(result);
