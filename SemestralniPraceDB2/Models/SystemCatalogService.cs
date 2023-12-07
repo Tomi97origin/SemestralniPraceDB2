0c8900b2d231c2d77f5a747e641705582815c5b1
Tomáš Patzák <st67084@upce.cz>
2023-12-07 19:56:36 +0100

    Smazany nepouzivany kod

--- SemestralniPraceDB2/Models/SystemCatalogService.cs
+++ SemestralniPraceDB2/Models/SystemCatalogService.cs
@@ -15,32 +15,6 @@ namespace SemestralniPraceDB2.Models
         public static List<DBTable> GetAllTables()
         {
             var tables = new List<DBTable>();
-            /*using (OracleConnection connection = DatabaseConnector.GetConnection())
-            {
-                connection.Open();
-                using (OracleTransaction transaction = connection.BeginTransaction())
-                {
-                    try
-                    {
-                        string sql = "SELECT t.* FROM user_tables t";
-                        List<OracleParameter> prm = new();
-                        var result = DatabaseConnector.ExecuteCommandQueryForTransactionAsync(sql, prm, connection, MapOracleResultToTable).Result;
-                        foreach (DBTable table in result)
-                        {
-                            sql = "SELECT count(*) as  row_count  FROM " + table.TableName + " t";
-                            prm = new();
-                            var res = DatabaseConnector.ExecuteCommandQueryForTransactionAsync(sql, prm, connection, (reader) => reader.GetInt32("row_count")).Result;
-                            table.RowCount = res[0];
-                        }
-                        tables.AddRange(result);
-                        return tables;
-                    }
-                    catch (Exception ex)
-                    {
-                        return tables;
-                    }
-                }
-            }*/
             string sql = "SELECT table_name,ROWCOUNT(table_name) as row_count FROM user_tables t";
             List<OracleParameter> prm = new();
             var result = DatabaseConnector.ExecuteCommandQueryAsync(sql, prm, MapOracleResultToTable).Result;
